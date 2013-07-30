326602918ba054973d5f0482ce6ae6fabefeadd7
Lucas O'Conor <lvoconor@gmail.com>
2013-07-30 15:32:39 -0700

    fixed camera initalization issues

--- public/gl_app/javascripts/App.js
+++ public/gl_app/javascripts/App.js
@@ -79,12 +79,12 @@ function (Constants, Camera, FPCamera, Renderer, AssetManager, ModelInstance, Sc
        
         
         this.scene = new Scene();
-	//this.camera = new FPCamera(this.scene);
+	this.camera = new FPCamera(this.scene);
 	
 	//this.UpdateView();
 /*
-	        var cameraData = JSON.parse("{\"eyePos\":{\"0\":3.776055335998535,\"1\":-187.77793884277344,\"2\":164.77069091796875,\"buffer\":{\"byteLength\":12},\"length\":3,\"byteOffset\":0,\"byteLength\":12},\"lookAtPoint\":{\"0\":0,\"1\":1,\"2\":0,\"buffer\":{\"byteLength\":12},\"length\":3,\"byteOffset\":0,\"byteLength\":12},\"upVec\":{\"0\":-0.01314918976277113,\"1\":0.6573730707168579,\"2\":0.7534525990486145,\"buffer\":{\"byteLength\":12},\"length\":3,\"byteOffset\":0,\"byteLength\":12},\"lookVec\":{\"0\":-0.015068011358380318,\"1\":0.7533015012741089,\"2\":-0.6575027108192444,\"buffer\":{\"byteLength\":12},\"length\":3,\"byteOffset\":0,\"byteLength\":12},\"leftVec\":{\"0\":-0.9998010993003845,\"1\":-0.019998691976070404,\"2\":0,\"buffer\":{\"byteLength\":12},\"length\":3,\"byteOffset\":0,\"byteLength\":12}}");
         $.extend(this.camera, cameraData);
+	        var cameraData = JSON.parse("{\"eyePos\":{\"0\":3.776055335998535,\"1\":-187.77793884277344,\"2\":164.77069091796875,\"buffer\":{\"byteLength\":12},\"length\":3,\"byteOffset\":0,\"byteLength\":12},\"lookAtPoint\":{\"0\":0,\"1\":1,\"2\":0,\"buffer\":{\"byteLength\":12},\"length\":3,\"byteOffset\":0,\"byteLength\":12},\"upVec\":{\"0\":-0.01314918976277113,\"1\":0.6573730707168579,\"2\":0.7534525990486145,\"buffer\":{\"byteLength\":12},\"length\":3,\"byteOffset\":0,\"byteLength\":12},\"lookVec\":{\"0\":-0.015068011358380318,\"1\":0.7533015012741089,\"2\":-0.6575027108192444,\"buffer\":{\"byteLength\":12},\"length\":3,\"byteOffset\":0,\"byteLength\":12},\"leftVec\":{\"0\":-0.9998010993003845,\"1\":-0.019998691976070404,\"2\":0,\"buffer\":{\"byteLength\":12},\"length\":3,\"byteOffset\":0,\"byteLength\":12}}");
 */	
 
         this.renderer = new Renderer(canvas, this.scene);
@@ -146,7 +146,7 @@ function (Constants, Camera, FPCamera, Renderer, AssetManager, ModelInstance, Sc
 				if(this.isValidCameraPosition(pos)){
 					//var upVec = vec3.create([0,0,1]);
 					var eyePos = vec3.create([pos[0], pos[1] ,50]);
-					console.log(eyePos);
+					//console.log(eyePos);
 					//var lookAt = vec3.create([1,1,1]); 
 					this.camera.Reset(eyePos);
 					return; 
@@ -271,25 +271,27 @@ function (Constants, Camera, FPCamera, Renderer, AssetManager, ModelInstance, Sc
     {
         this.LoadScene(
         function() { // on success finish up some setup
-			this.camera = new FPCamera(this.scene);
+			//this.camera = new FPCamera(this.scene);
 			this.camera.UpdateSceneBounds(this.scene.Bounds());
 			this.SetCamera();
 			this.camera.SaveStateForReset();
 			this.AttachViewSelectionEventHandlers();
-            this.undoStack.clear();
-            this.renderer.postRedisplay();
+            		this.undoStack.clear();
+			this.UpdateView();
+
         }.bind(this),
         function() { // on failure create an empty room
             this.assman.GetModel('room', function (model)
             {
                 this.scene.Reset(new ModelInstance(model, null));
-		this.camera = new FPCamera(this.scene);
+		//this.camera = new FPCamera(this.scene);
                 this.camera.UpdateSceneBounds(this.scene.Bounds());
 		this.SetCamera();
                 this.camera.SaveStateForReset();
 		this.AttachViewSelectionEventHandlers();
                 this.undoStack.clear();
-                this.renderer.postRedisplay();
+		this.UpdateView();
+
             }.bind(this));
         }.bind(this)
         );
