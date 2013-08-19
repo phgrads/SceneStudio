81b98cebd3296f10e55d4c25d3a850152fa50471
Manolis Savva <manolis.savva@gmail.com>
2013-08-19 10:26:47 -0700

    Fix bug in globalViewData assignmentId and update test experiment controller code for now Mturk backend logic

--- app/controllers/experiments/test_controller.rb
+++ app/controllers/experiments/test_controller.rb
@@ -2,33 +2,13 @@ class Experiments::TestController < ApplicationController
   include MturkHelper
 
   before_filter :load_new_tab_params, only: [:index]
-  before_filter :mturk_user
-  before_filter :get_scene
-  layout 'webgl_viewport', only: [:view, :index]
+  layout 'webgl_viewport'
 
   def index
-  	#render 'index'
-  	render '/scenes/view'
-  	#redirect_to action:'view', id:@scene.id #Can't just call render because you need the correct id
-  	#render 'view'
-  end
-
-  def view
-  	#render 'scenes/view'
-  end
-
-  def update
-
-  end
-
-  private
-
-  def get_scene
-  	@scene = @mturk_user.scenes.first
-  end
-
-  def mturk_user
-  	@mturk_user = User.find_by_name("worker")
+    if not @via_turk then
+      @task = MtTask.find_by_name!("test")
+    end
+    @scene = Scene.find(104)
+	render 'scenes/view'
   end
 end
-
---
+++ app/helpers/experiments/test_helper.rb
@@ -0,0 +1,2 @@
+module Experiments::TestHelper
+end
