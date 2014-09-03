b6d3a506afb1788a34482a8443474a687fbebeab
Angel X Chang <angelx@gmail.com>
2014-09-03 13:48:17 -0700

    Start fleshing out desc2scene mturk task.

--- app/controllers/experiments/desc2scene_controller.rb
+++ app/controllers/experiments/desc2scene_controller.rb
@@ -2,11 +2,26 @@ class Experiments::Desc2sceneController < ApplicationController
   include MturkHelper
 
   before_filter :load_new_tab_params, only: [:index]
+  layout 'webgl_viewport'
 
   def index
     if not @via_turk then
+
       @task = MtTask.find_by_name!("desc2scene")
+      @scene = @task.user.scenes.build({
+                                           name: Time.now.to_s,
+                                       })
+    else
+      @scene = @task.user.scenes.build({
+                                           name: params[:assignmentId],
+                                       })
     end
-    render "experiments/desc2scene/index", layout: false
+    if @scene.save
+      flash[:success] = 'Scene created!'
+      render "experiments/desc2scene/index", layout: true
+    else
+      flash[:error] = @scene.errors.full_messages.to_sentence
+    end
   end
+
 end
