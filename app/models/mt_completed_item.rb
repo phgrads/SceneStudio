2fd9030b1cf7cc320f4a8dd071a15f9e76142cc3
Manolis Savva <manolis.savva@gmail.com>
2014-09-10 14:35:04 -0700

    Remove deleted field from MtCompletedItem model

--- app/models/mt_completed_item.rb
+++ app/models/mt_completed_item.rb
@@ -1,5 +1,5 @@
 class MtCompletedItem < ActiveRecord::Base
   belongs_to :mt_task
   belongs_to :mt_worker
-  attr_accessible :data, :mt_assignment_id, :mt_condition, :mt_hit_id, :mt_item
+  attr_accessible :data, :mt_assignment_id, :mt_condition, :mt_item
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140910171933) do
+ActiveRecord::Schema.define(:version => 20140910183113) do
 
   create_table "identities", :force => true do |t|
     t.string   "name"
