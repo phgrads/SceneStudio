3b67b75cbca18b1ec5ecd7132bbcdbe5d9c76521
msavva <manolis.savva@gmail.com>
2014-09-09 20:08:20 -0700

    Add hack to get base url of assets to be correct

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,10 @@ SceneStudio::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # TODO: Get rid of these hacks
+  # Terrible hack to explicitly set relative url root for sprockets require statements
+  config.relative_url_root = '/scenestudio'
+  # Another terrible hack that fixes require.js-rails ignoring the above
+  config.requirejs.run_config['baseUrl'] = '/scenestudio/assets'
 end
