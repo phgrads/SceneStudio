5eefe27bd3262037f4a2c7a5008a07719974ecc7
Angel X Chang <angelx@gmail.com>
2014-09-23 23:38:11 -0700

    Show time in results view for desc2scene and image2scene.

--- app/models/completed_items_view.rb
+++ app/models/completed_items_view.rb
@@ -1,7 +1,7 @@
 class CompletedItemsView < ActiveRecord::Base
   #self.table_name = 'completed_items_view'  # for rails >= 3.2
   self.set_table_name 'completed_items_view' # for rails <= 3.1
-  attr_readonly :data, :taskId, :taskName, :workerId, :condition, :item
+  attr_readonly :data, :taskId, :taskName, :workerId, :condition, :item, :created_at, :updated_at
   dragonfly_accessor :preview
 
   belongs_to :mt_task, :foreign_key => 'taskId'
