089ca6ecd067f4ce0b5929180fac26b7f177a5d8
Angel X Chang <angelx@gmail.com>
2014-09-29 15:36:47 -0700

    Add logic to prevent worker from doing same item again.

--- app/helpers/experiments/experiments_helper.rb
+++ app/helpers/experiments/experiments_helper.rb
@@ -19,24 +19,76 @@ module Experiments::ExperimentsHelper
   end
 
   def load_and_select_random(inputFile, doneFile, doneThreshold, n)
+    # Figure out task and worker id
+    taskId = @task.id
+    workerId = @worker ? @worker.mtId : ''
+    # Load entries from file
     all_entries = load_entries(inputFile)
+    # Filter all entries by entries that were deemed overall "DONE"
     if (doneFile != nil && doneFile != "")
-      done_entries = load_entries_done(doneFile)
+      # Use file to figure out what items are done
       if (doneThreshold != nil && doneThreshold > 0)
+        # Load done entries from file
+        done_entries = load_entries_done(doneFile)
         # Only consider those items with a count greater than the threshold to be done
         all_done_count = done_entries.size
         done_entries = done_entries.select{ |x| x[:count].to_i >= doneThreshold }
         done_count = done_entries.size
         logger.debug "Keep done " + done_count.to_s + " of " + all_done_count.to_s + " (threshold " + doneThreshold.to_s + ")"
+        # Remove done from all
+        done = done_entries.map{ |x| x[:id] }.to_set
+        entries = all_entries.select{ |x| !done.include?(x[:id]) }
+      else
+        entries = all_entries
       end
-      # Remove done from all
-      done = done_entries.map{ |x| x[:id] }.to_set
-      entries = all_entries.select{ |x| !done.include?(x[:id]) }
     else
-      entries = all_entries
+      if (doneThreshold != nil && doneThreshold > 0)
+        # Figure out count of done items from database
+        # done_entries is a hash of item id to count
+        done_entries = count_completed_entries(taskId)
+
+        # Only consider those items with a count greater than the threshold to be done
+        all_done_count = done_entries.size
+        done_entries = done_entries.select{ |k,v| v.to_i >= doneThreshold }
+        done_count = done_entries.size
+        logger.debug "Keep done " + done_count.to_s + " of " + all_done_count.to_s + " (threshold " + doneThreshold.to_s + ")"
+        done = done_entries.map{ |k,v| k }.to_set
+
+        # Remove done from all
+        entries = all_entries.select{ |x| !done.include?(x[:id]) }
+      else
+        entries = all_entries
+      end
     end
+    # Filter by entries that were already done for the worker
+    entries = filter_worker_completed_entries(entries, taskId, workerId)
+
+    # Do random selection from final remaining entries
     logger.debug "Selecting " + n.to_s + " random entries from " + entries.size.to_s + " entries"
     select_random(entries, n)
+  end
+
+  def filter_worker_completed_entries(entries, taskId, workerId)
+    worker_completed = load_worker_completed_entries(taskId, workerId)
+    logger.debug "Worker " + workerId.to_s + " has already completed " + worker_completed.size.to_s + " entries"
+    worker_done = worker_completed.map{ |x| x[:item] }.to_set
+    entries.select{ |x| !worker_done.include?(x[:id]) }
+  end
+
+  def load_worker_completed_entries(taskId, workerId)
+    # Load entries that were already done by the worker for this task
+    CompletedItemsView.where('taskId = ? AND workerId = ?', taskId, workerId)
+  end
+
+  def load_completed_entries(taskId)
+    # Load entries that were already done for this task
+    CompletedItemsView.where('taskId = ?', taskId)
+  end
+
+  def count_completed_entries(taskId)
+    # Count entries already done for this task
+    # TODO: Only count approved
+    CompletedItemsView.group(:item).where('taskId = ?', taskId).count()
   end
 
   def load_entries(file)
--- app/models/completed_items_view.rb
+++ app/models/completed_items_view.rb
@@ -1,6 +1,5 @@
 class CompletedItemsView < ActiveRecord::Base
-  #self.table_name = 'completed_items_view'  # for rails >= 3.2
-  self.set_table_name 'completed_items_view' # for rails <= 3.1
+  self.table_name = 'completed_items_view'  # for rails >= 3.2
   attr_readonly :data, :taskId, :taskName, :workerId, :condition, :item, :created_at, :updated_at
   dragonfly_accessor :preview
 
