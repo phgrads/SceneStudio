8f2e031dcbd760e279c169c12b177f08898842ad
Angel X Chang <angelx@gmail.com>
2014-10-07 21:28:28 -0700

    Fix sSwfPath for TableTools.

--- app/assets/javascripts/experiments/results.js
+++ app/assets/javascripts/experiments/results.js
@@ -4,11 +4,13 @@
 define(function(require) {
   require(['jquery.datatables.bootstrap','jquery.datatables.tabletools'], function() {
     $(document).ready(function() {
+      var sSwfPath = window.globals.base_url + '/swf/copy_csv_xls_pdf.swf';
       $('#resultsTable').dataTable({
         'dom': 'T<"clear">lfrtip',
-        'order': [[ 0, "desc" ]],
+        'order': [ 0, "desc" ],
         tableTools: {
-          "sSwfPath": "/swf/copy_csv_xls_pdf.swf"
+//          "sSwfPath": sSwfPath
+          "sSwfPath": '../../../swf/copy_csv_xls_pdf.swf'
         }
       });
     } );
